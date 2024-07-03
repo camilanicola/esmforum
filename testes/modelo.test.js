f3dea523598691015ff7e55a5dc0b19bca35ae37
camilanicola <49569244+camilanicola@users.noreply.github.com>
2024-07-03 13:31:56 -0300

    Update modelo.test.js

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,20 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+test('Testando respostas por perguntas cadastradas',() => {
+  const idPergunta = modelo.cadastrar_pergunta('Quanto é 5 + 5?');
+  const idResposta = modelo.cadastrar_resposta(idPergunta, '10');
+  const respostas = modelo.get_respostas(idPergunta);
+
+  expect(respostas.length).toBe(1);
+  expect(respostas[0].texto).toBe('10');
+  expect(respostas[0].id_pergunta).toBe(idPergunta);
+  
+});
+test('Testando obtenção de uma pergunta por ID', () => {
+  const idPergunta = modelo.cadastrar_pergunta('Qual é a capital da França?');
+  const pergunta = modelo.get_pergunta(idPergunta);
+
+  expect(pergunta.id_pergunta).toBe(idPergunta);
+  expect(pergunta.texto).toBe('Qual é a capital da França?');
+});
